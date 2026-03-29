2058fa429b5a85b019da0815810266d8bcc751e8
Arthur <arthurbruno489@gmail.com>
2026-03-28 22:21:38 -0300

    chore: initialize Spring Boot project with PostgreSQL and dotenv

---
+++ src/main/java/com/api/SistemaMEI/SistemaMeiApplication.java
@@ -0,0 +1,13 @@
+package com.api.SistemaMEI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMeiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMeiApplication.class, args);
+	}
+
+}
