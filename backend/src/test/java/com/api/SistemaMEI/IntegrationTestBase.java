83ff5597ccd3605e225b5960842dfb1b261bef29
Arthur <arthurbruno489@gmail.com>
2026-04-05 22:42:12 -0300

    chore: adiciona fluxo dev.sh com H2 e testes locais

--- backend/src/test/java/com/api/SistemaMEI/IntegrationTestBase.java
+++ backend/src/test/java/com/api/SistemaMEI/IntegrationTestBase.java
@@ -2,27 +2,8 @@ package com.api.SistemaMEI;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
-@Testcontainers
 @ActiveProfiles("test")
 public abstract class IntegrationTestBase {
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17-alpine")
-            .withDatabaseName("sistemamei-test")
-            .withUsername("test")
-            .withPassword("test");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
 }
